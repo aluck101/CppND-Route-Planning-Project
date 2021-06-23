47d1e1473893571997b5e7e2351d26a5414bbbb9
Tunde Oluwayemi Aluko <tunde-oluwayemi.aluko@stud.hshl.de>
2021-06-23 18:30:32 +0200

    fix gitignore

--- cmake-build-debug/thirdparty/googletest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/aluck/CLionProjects/OpenStreetMap/CppND-Route-Planning-Project/thirdparty/googletest
-# Build directory: /home/aluck/CLionProjects/OpenStreetMap/CppND-Route-Planning-Project/cmake-build-debug/thirdparty/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
